bca42e13e0b5eb24392818b0e06ad49befc56def
jkoni613 <jkoni613@gmail.com>
2015-10-04 09:50:33 +0000

    user item

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Monolist"
       redirect_to @user, notice: "会員登録が完了しました"
     else
       render 'new'
@@ -24,7 +25,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
